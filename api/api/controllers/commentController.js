1d3185ce196d2442b9a69f5f027d6059a57c3b01
George Xiao <g5xiao@edu.uwaterloo.ca>
2020-04-25 15:25:03 -0400

    ugh

--- api/api/controllers/commentController.js
+++ api/api/controllers/commentController.js
@@ -1,24 +1,44 @@
 'use strict';
+var cloneDeep = require('lodash.clonedeep');
 
 var mongoose = require('mongoose'),
     Post = mongoose.model('Posts'),
     Comment = mongoose.model('Comments'),
     User = mongoose.model('Users');
 
-exports.listAllComments = function(req, res) {
+var postComments = [];
+async function processCommentIds(commentIdArray, res) {
+    for (let i = 0; i < commentIdArray.length; i++) {
+        await Comment.findById(commentIdArray[i], async function (err, comment) {
+            if (err) {
+                return handleError(err);
+            }
+            var commentUserId = comment.userId, commentBody = comment.body, commentTimestamp = comment.dateCreated;
+            await User.findById(commentUserId.toString(), async function (err, user) {
+                if (err) {
+                    return handleError(err);
+                } else {
+                    // return 0;
+                    postComments.push({username: user.username, dateCreated: commentTimestamp, body: commentBody, userId: commentUserId});
+                    res.json(postComments)
+                }
+            }).exec();
+        }).exec();
+    }
+}
+
+exports.listAllComments = function (req, res) {
+    var response = [], commentID;
     var comment_ids = [];
-    Post.findById(req.params.postId, function (err, post) {
+    var postObj;
+    Post.findById(req.params.postId, async function (err, post) {
         if (err) return handleError(err);
-        comment_ids.push(post.commentIds);
+        await processCommentIds(post.commentIds, res);
+        res.json(postComments)
     });
-    
-    // Fix this
-    // for (let i = 0; i < comment_ids.length; i++) {
-    //     Comment.findById(comment_ids[i], "body userId")
-    // }
 };
 
-exports.createComment = function(req, res) {
+exports.createComment = function (req, res) {
     // comment id for new comment
     var commentId = mongoose.Types.ObjectId();
     Post.findById(req.params.postId, function (err, post) {
@@ -26,7 +46,7 @@ exports.createComment = function(req, res) {
             return handleError(err);
         }
         post.commentIds.push(commentId);
-        post.save(function(err, post) {
+        post.save(function (err, post) {
             if (err)
                 res.send(err);
         });
@@ -34,7 +54,7 @@ exports.createComment = function(req, res) {
 
     var newComment = new Comment(req.body);
     newComment._id = commentId;
-    newComment.save(function(err, comment) {
+    newComment.save(function (err, comment) {
         if (err)
             res.send(err);
         res.json(comment);
