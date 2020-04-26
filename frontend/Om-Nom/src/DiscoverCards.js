a663fb26b61a10bef8672fa92820ec18debd6ece
Jake Xia <jakexia72@hotmail.com>
2020-04-26 04:06:54 -0400

    fixed issues with not loading

--- frontend/Om-Nom/src/DiscoverCards.js
+++ frontend/Om-Nom/src/DiscoverCards.js
@@ -4,28 +4,38 @@ import { Avatar, Card, CardHeader, CardContent, CardActions, CardMedia, IconButt
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import Grid from '@material-ui/core/Grid';
 
-const DiscoverCards = props => {
-    const { profilePicture, title, photoLinks } = props;
+class DiscoverCards extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // post: {}
+        };
+    //  const { profilePicture, title, photoLinks } = props;
+    }
 
-    return (
-        <Card>
-            <CardMedia style={{ height: "200px" }} image={photoLinks[0]} title="Cauliflower Pizza" />
-            <Grid container spacing={1}>
-                <Grid item xs={8}>
-                    <CardHeader
-                        avatar={<Avatar src={profilePicture} />}
-                        title={title} />
+    
+    render() {
+        return (
+            <Card>
+                <CardMedia style={{ height: "200px" }} image={this.props.photoLinks[0]} title="Cauliflower Pizza" />
+                <Grid container spacing={1}>
+                    <Grid item xs={8}>
+                        <CardHeader
+                            avatar={<Avatar src={this.props.profilePicture} />}
+                            title={this.props.title} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="add to favorites">
+                                <FavoriteIcon />
+                            </IconButton>
+                        </CardActions>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                    <CardActions disableSpacing>
-                        <IconButton aria-label="add to favorites">
-                            <FavoriteIcon />
-                        </IconButton>
-                    </CardActions>
-                </Grid>
-            </Grid>
-        </Card>
-    );
+            </Card>
+        );
+    }
+    
 
 }
 
